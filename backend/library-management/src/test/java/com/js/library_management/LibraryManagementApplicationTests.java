e490dfe99199f4d7f6ea854873e63d35fd7151d4
Yamini Challagundla <srujanayamini8@gmail.com>
2025-08-22 20:55:19 +0530

    Spring added

---
+++ backend/library-management/src/test/java/com/js/library_management/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.library_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
